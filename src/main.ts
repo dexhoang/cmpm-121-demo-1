2a8b5fc79d815e2f2301cffd9ca7ff5678c87225
dexhoang <dhhoang@ucsc.edu>
2024-10-15 08:44:51 -0800

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,15 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//create button
+const button = document.createElement('button');
+button.textContent = '🥕';
+document.body.appendChild(button);
+
+//checks if button was clicked
+if (button) {
+    button.addEventListener("click", () => {
+        console.log("Button was clicked!");
+    })
+}
